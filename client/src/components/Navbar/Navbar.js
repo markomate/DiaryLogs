cb35450cfe80ca2a58875cfcfbe474a1dcdb3bc5
markomate <GCAS022005@coderacademy.edu.au>
2022-07-26 04:30:32 +1000

    Started on Navbar and routes

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import {Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink} from './NavbarElements'
+
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>
+          <img src={require('../../images/Logo.png')} alt='logo' style={{height: '10vh'}}/>
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/signup' activeStyle>
+            Sign Up
+          </NavLink>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavMenu>
+        {/* <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn> */}
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
