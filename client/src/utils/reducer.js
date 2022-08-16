ca7ac12e8253f3dfb8cd939f9caa6c63b4b92218
markomate <GCAS022005@coderacademy.edu.au>
2022-08-17 00:30:24 +1000

    Trying to resolve issues pt.2

--- client/src/utils/reducer.js
+++ client/src/utils/reducer.js
@@ -37,7 +37,6 @@ export const reducer = (state, action) => {
         ...state,
         logList: [action.data, ...state.logList],
       };
-<<<<<<< HEAD
     }
     case "editLog": {
       // edits the selected log and removes the original from the list
@@ -62,29 +61,3 @@ export const reducer = (state, action) => {
       return state;
   }
 };
-=======
-    }
-    case "editLog": {
-      // edits the selected log and removes the original from the list
-      let updatedList = state.logList.filter(
-        (log) => log.id !== action.data.id
-      );
-      return {
-        ...state,
-        logList: [action.data, ...updatedList],
-      };
-    }
-    case "deleteLog": {
-      // deletes a log from the logList array based on it's ID
-      // updates logList with the new array
-      let updatedList = state.logList.filter((log) => log.id !== action.data);
-      return {
-        ...state,
-        logList: updatedList,
-      };
-    }
-    default:
-      return state;
-  }
-};
->>>>>>> fb5e43021833e37b55161eb532a89258640e5bc8
